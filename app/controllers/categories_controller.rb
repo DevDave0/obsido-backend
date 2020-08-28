0ec305d142484306fd67bba0b78aa51a3faa8fee
devdave0 <dbchung24@gmail.com>
2020-08-27 23:26:19 -0400

    added skip before action to category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
 
+    skip_before_action :logged_in?, only: [:create, :index]
+
     def index 
         @categories = Category.all 
         render json: @categories
