7e05542cb9ae4f23d2905cef591ccbe7fe0bce9a
devdave0 <dbchung24@gmail.com>
2020-08-27 22:57:18 -0400

    added actions to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+    def create 
+        user = User.new(user_params)
+
+        if user.valid?
+            user.save 
+            render json: {user: user}
+        else 
+            render json: {error: "Failed to create a user"}
+        end 
+
+    end 
+
+
+
+    private 
+
+    def user_params 
+        params.permit(:name, :password)
+    end 
+
 end
